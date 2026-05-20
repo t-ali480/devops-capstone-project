d3686339adb391438be203af3e88c2837d9d4ec8
t-ali380 <dev@tali8.net>
2026-05-20 15:47:19 -0400

    New Fix on Ci Task

--- service/routes.py
+++ service/routes.py
@@ -64,6 +64,7 @@ def create_accounts():
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
+    
     """
     List all Accounts
     This endpoint will list all Accounts
@@ -73,7 +74,6 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
-
 
 ######################################################################
 # READ AN ACCOUNT
@@ -97,6 +97,7 @@ def get_accounts(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
+
     """
     Update an Account
     This endpoint will update an Account based on the posted data
@@ -115,6 +116,7 @@ def update_accounts(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
+
     """
     Delete an Account
     This endpoint will delete an Account based on the account_id that is requested
@@ -131,6 +133,7 @@ def delete_accounts(account_id):
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
+
     content_type = request.headers.get("Content-Type")
     if content_type and content_type == media_type:
         return
